41470cc69a789a9db35053521d6dcf44edd320f1
Fábio Correia <fabio.correia@moloni.com>
2023-04-17 15:05:26 +0100

    Release 2.5.1

--- moloni.php
+++ moloni.php
@@ -27,7 +27,7 @@ class Moloni extends Module
         $this->name = 'moloni';
         $this->tab = 'administration';
         $this->need_instance = 1;
-        $this->version = '2.5.0';
+        $this->version = '2.5.1';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->author = 'Nuno Almeida';
         $this->bootstrap = true;
