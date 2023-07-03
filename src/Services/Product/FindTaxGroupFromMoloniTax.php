ee2eb07e1d1b9cba16f0ca53137e2dfeb67a043d
Fábio Correia <fabio.correia@moloni.com>
2023-07-03 10:30:31 +0100

    Import taxes from Moloni when creating products in Prestashop

---
+++ src/Services/Product/FindTaxGroupFromMoloniTax.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Moloni\Services\Product;
+
+use Country;
+use TaxRulesGroup;
+
+class FindTaxGroupFromMoloniTax
+{
+    private $moloniTax;
+
+    public function __construct(array $moloniTax)
+    {
+        $this->moloniTax = $moloniTax;
+    }
+
+    public function handle(): int
+    {
+        $fiscalZone = $this->moloniTax['fiscalZone'] ?? 'pt';
+        $countryId = Country::getByIso($fiscalZone);
+        $value = (float)($this->moloniTax['value'] ?? 0);
+
+        $taxes = array_reverse(TaxRulesGroup::getAssociatedTaxRatesByIdCountry($countryId), true);
+
+        foreach ($taxes as $id => $tax) {
+            if ($value === (float)$tax) {
+                $taxRuleGroupObject = new TaxRulesGroup($id);
+
+                if (!empty($taxRuleGroupObject->deleted) || empty($taxRuleGroupObject->active)) {
+                    continue;
+                }
+
+                return $id;
+            }
+        }
+
+        return 0;
+    }
+}
