3ba9d55cb858db6f3e32e77b9db28aec8d7ea945
Fábio Correia <fabio.correia@moloni.com>
2025-06-17 11:52:28 +0100

    Updates package reference

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'moloni-pt/prestashop',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f0cc397c5bb33ecb82e4a183c9a9ff6d2a2380f2',
+        'reference' => 'c17ef1f2f331c5e223a8a391b5e1d330ef4c2fca',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'moloni-pt/prestashop' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'f0cc397c5bb33ecb82e4a183c9a9ff6d2a2380f2',
+            'reference' => 'c17ef1f2f331c5e223a8a391b5e1d330ef4c2fca',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
