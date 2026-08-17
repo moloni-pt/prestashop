3aeff5055b6c572ec8176bb1641a4994312cfea8
Fábio Correia <fabio.correia@moloni.com>
2026-08-17 11:46:07 +0100

    Bump lock

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'moloni-pt/prestashop',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '12981cb9b0ad473e9d4b63740cc543702166c249',
+        'reference' => '6e47054186bee68adadba3a100c69607c3b4ae53',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'moloni-pt/prestashop' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '12981cb9b0ad473e9d4b63740cc543702166c249',
+            'reference' => '6e47054186bee68adadba3a100c69607c3b4ae53',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
